a323a9b976516ce5455f54e11a20ef9523772e83
Connall1234 <>
2024-01-21 00:56:38 +0000

    mplement Board class for game board management.

--- run.py
+++ run.py
@@ -11,3 +11,34 @@ boat_computer = []
 
 """List to track the computer's guesses"""
 computer_poss_guesses = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+
+
+class Board:
+    """
+    Class to create and manage the game board.
+    """
+    def __init__(self, board_size, whos_playing, type):
+        self.board_size = board_size
+        self.whos_playing = whos_playing
+        self.type = type
+
+    """Function to print the game boards with misses, hits, and ships."""
+    def print_board(self, miss_ship, hit_ship, boat):
+        place = 0
+        print("__________________")
+        print(f"\n{self.whos_playing}'s board\n")
+        print("__________________")
+        print("     0  1  2  3  4")
+        for point in range(5):
+            row = ""
+            for y in range(5):
+                ch = " _ "
+                if place in miss_ship:
+                    ch = " x "
+                elif place in hit_ship:
+                    ch = " o "
+                elif self.type == "Type A" and place in boat:
+                    ch = " @ "
+                row = row + ch
+                place = place + 1
+            print(point, " ", row)
