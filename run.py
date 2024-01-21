c7b2a045ab82793e8b7980478235dc1408c66408
Connall1234 <>
2024-01-21 00:55:56 +0000

    Initial code structure and global lists for player and computer ships.

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+# Player's global lists for misses, hits, and boats
+miss_ship_player = []
+hit_ship_player = []
+boat_player = []
+
+# Computer's global lists for misses, hits, and boats
+miss_ship_computer = []
+hit_ship_computer = []
+boat_computer = []
+
+"""List to track the computer's guesses"""
+computer_poss_guesses = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
